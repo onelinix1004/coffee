5c1b5950b55404b9ed0606f0f91872a99eae081c
HoaAlune <nguyenhoa104004@gmail.com>
2025-03-11 11:11:57 +0700

    Add enum status and role for model order and user

---
+++ src/main/java/com/project/coffee/enums/Role.java
@@ -0,0 +1,5 @@
+package com.project.coffee.enums;
+
+public enum Role {
+    Admin, User
+}
---
+++ src/main/java/com/project/coffee/enums/Status.java
@@ -0,0 +1,5 @@
+package com.project.coffee.enums;
+
+public enum Status {
+    Pending, Shipped, Delivered, Canceled
+}
